cf0229683cd28cf81077756927b9bd7f8cfa8b45
Gorra618 <messier57lyr@gmail.com>
2025-03-27 10:02:33 -0300

    Version_2

--- modifyCodeBase.py
+++ modifyCodeBase.py
@@ -1,3 +1,8 @@
+#3 INCOMPLETO
+#4 INCOMPLETO
+#5 INCOMPLETO
+#6 INCOMPLETO 
+
 def append_task(tasks,task, limit_date, priority, state):
     new_task = {"Tarea" : task, "Fecha_Limite" : limit_date, "Prioridad" : priority, "Estado" : state}
     tasks.append(new_task)
@@ -5,32 +10,12 @@ def append_task(tasks,task, limit_date, priority, state):
 def show_task(tasks):
     if not tasks:
         print("No hay tareas")
-    elif tasks.state == "Hecho":
+    else:
         for i, task in enumerate(tasks,1):
             print(f"\nTarea {i}:")
             for clave, value in task.items():
                 print(f"{clave} : {value}")
 
-
-def marcar_completada(state):
-    while state != "Incompleto" or state != "Hecho":
-        if state == "Incompleto":
-            state = "Incompleto"
-            break
-        elif state == "Hecho":
-            state = "Hecho"
-            break
-        else: #ERROR No aplica el nuevo estado
-            print("ESTADO INVALIDO")
-            state2 = input("Ingrese estado: ")
-            if state2 == "Incompleto":
-                state = state2
-                break
-            elif state2 == "Hecho":
-                state = state2
-                break
-    return state
-            
 if __name__ == "__main__":
     task_list = []
     while True:
@@ -45,7 +30,6 @@ if __name__ == "__main__":
             new_limit_task = input("Ingrese fecha limite:")
             new_priority = input("Ingrese prioridad:")
             new_state = input("Ingrese estado de la tarea (Hecho o Incompleto):")
-            marcar_completada(new_state)
             append_task(task_list,new2_task,new_limit_task,new_priority,new_state)
             
         elif choice == "2":
